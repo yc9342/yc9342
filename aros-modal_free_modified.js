8b2594b9e9f0ff65ed406223ad9949ca80de540d
yc9342 <yc9342yc@gmail.com>
2024-06-04 11:17:14 +0900

    Add files via upload

---
+++ aros-modal_free_modified.js
@@ -0,0 +1,62 @@
+function _0x1994(_0x3991d9, _0x333045) {
+    const _0x5275f3 = _0x5275();
+    return _0x1994 = function (_0x1994d0, _0x4a2922) {
+        _0x1994d0 = _0x1994d0 - 0x1c4;
+        let _0x3d730f = _0x5275f3[_0x1994d0];
+        return _0x3d730f;
+    }, _0x1994(_0x3991d9, _0x333045);
+}
+
+const _0x4b0117 = _0x1994;
+
+(function (_0x476483, _0x324922) {
+    const _0x41cd4d = _0x1994,
+        _0x405392 = _0x476483();
+    while (!![]) {
+        try {
+            const _0x469374 = parseInt(_0x41cd4d(0x1e2)) / 0x1 * (-parseInt(_0x41cd4d(0x1cf)) / 0x2) + -parseInt(_0x41cd4d(0x1de)) / 0x3 * (parseInt(_0x41cd4d(0x1c6)) / 0x4) + parseInt(_0x41cd4d(0x1cb)) / 0x5 + parseInt(_0x41cd4d(0x1e4)) / 0x6 + parseInt(_0x41cd4d(0x1e6)) / 0x7 * (-parseInt(_0x41cd4d(0x1ce)) / 0x8) + parseInt(_0x41cd4d(0x1d6)) / 0x9 + -parseInt(_0x41cd4d(0x1d5)) / 0xa * (-parseInt(_0x41cd4d(0x1e3)) / 0xb);
+            if (_0x469374 === _0x324922) break;
+            else _0x405392['push'](_0x405392['shift']());
+        } catch (_0x3e049f) {
+            _0x405392['push'](_0x405392['shift']());
+        }
+    }
+}(_0x5275, 0xa20e9));
+
+function _0x5275() {
+    const _0x1662cf = ['flex', 'scrollHeight', 'logo', 'aros-back', '244710lzevbS', 'innerHeight', 'img', '2248NHQcVh', '215482WbncLU', 'modalDismissedTime', 'appendChild', 'getTime', 'querySelector', 'scrollY', '50HPQfcX', '8930592emxnWD', 'addEventListener', 'scroll', 'aros-modal', 'DOMContentLoaded', 'createElement', 'alt', 'display', '626964JjNJiO', 'getElementById', 'setItem', 'innerHTML', '3wjAunh', '1398067IzAMHf', '6126900xqqHGe', 'className', '32207fJSVuJ', 'style', 'click', '8mnHDNu'];
+    _0x5275 = function () {
+        return _0x1662cf;
+    };
+    return _0x5275();
+}
+
+const modalStyles = '\x0a.aros-modal\x20{\x0a\x20\x20display:\x20none;\x20/*\x20Hidden\x20by\x20default\x20*/\x0a\x20\x20width:\x20100vw;\x0a\x20\x20height:\x20100vh;\x0a\x20\x20z-index:\x2099999;\x0a\x20\x20position:\x20fixed;\x0a\x20\x20top:\x200;\x0a\x20\x20left:\x200;\x0a}\x0a\x0a.aros-modal-bg\x20{\x0a\x20\x20position:\x20fixed;\x0a\x20\x20top:\x200;\x0a\x20\x20left:\x200;\x0a\x20\x20right:\x200;\x0a\x20\x20width:\x20100%;\x0a\x20\x20height:\x20100%;\x0a\x20\x20background-color:\x20rgba(0,\x200,\x200,\x200.8);\x0a\x20\x20display:\x20flex;\x0a\x20\x20justify-content:\x20center;\x0a\x20\x20align-items:\x20center;\x0a}\x0a\x0a.aros-modal-content\x20{\x0a\x20\x20\x20\x20position:\x20relative;\x0a\x20\x20\x20\x20display:\x20inline-block;\x0a\x20\x20\x20\x20overflow:\x20visible;\x20/*\x20Allow\x20content\x20to\x20overflow\x20*/\x0a}\x0a\x0a.aros-modal-content\x20.logo\x20{\x0a\x20\x20\x20\x20position:\x20absolute;\x0a\x20\x20\x20\x20top:\x2010px;\x0a\x20\x20\x20\x20right:\x2010px;\x20/*\x20Adjust\x20as\x20needed\x20*/\x0a\x20\x20\x20\x20width:\x2050px;\x0a\x20\x20\x20\x20height:\x20auto;\x0a\x20\x20\x20\x20z-index:\x2010;\x0a}\x0a\x0a.aros-modal-cancel-container\x20{\x0a\x20\x20position:\x20absolute;\x0a\x20\x20top:\x20-30px;\x0a\x20\x20right:\x20-30px;\x0a\x20\x20cursor:\x20pointer;\x0a}\x0a\x0a.aros-modal-cancel\x20{\x0a\x20\x20font-size:\x2030px;\x0a\x20\x20color:\x20white;\x0a}\x0a\x0a.aros-modal-container\x20{\x0a\x20\x20margin-top:\x200;\x0a\x20\x20position:\x20relative;\x0a\x20\x20border-radius:\x205px;\x0a\x20\x20max-width:\x201122px;\x0a\x20\x20background-color:\x20white;\x0a}\x0a\x0a.aros-modal-content\x20img\x20{\x0a\x20\x20display:\x20block;\x0a\x20\x20width:\x20500px;\x0a\x20\x20height:\x20500px;\x0a\x20\x20object-fit:\x20contain;\x0a\x20\x20background-color:\x20#4462f900;\x0a}\x0a\x0a.aros-modal-actions\x20{\x0a\x20\x20display:\x20flex;\x0a\x20\x20justify-content:\x20center;\x0a\x20\x20align-items:\x20center;\x0a\x20\x20padding:\x2036px\x200;\x0a\x20\x20column-gap:\x2015px;\x0a\x20\x20background-color:\x20white;\x0a}\x0a\x0a.aros-modal-button\x20{\x0a\x20\x20cursor:\x20pointer;\x0a\x20\x20display:\x20flex;\x20justify-content:\x20center;\x20align-items:\x20center;\x20color:\x20#fff;\x20font-family:\x20\x27SUIT\x20Bold\x27;\x20font-size:\x2018px;\x20font-weight:\x20600;\x20text-align:\x20center;\x20border-radius:\x205px;\x20width:\x20191px;\x20height:\x2051px;\x0a}\x0a\x0a.aros-modal-button.blue\x20{\x0a\x20\x20background-color:\x20#4461f9;\x20color:\x20#fff;\x20/*\x20Text\x20color\x20white\x20*/\x0a}\x0a\x0a.aros-modal-button.black\x20{\x0a\x20\x20background-color:\x20black;\x20color:\x20#fff;\x20/*\x20Text\x20color\x20white\x20*/\x0a}\x0a\x0a/*\x20Media\x20query\x20for\x20tablet\x20devices\x20*/\x0a@media\x20(max-width:\x20768px)\x20{\x0a\x20\x20.aros-modal-container\x20{\x0a\x20\x20\x20\x20max-width:\x20300px;\x20margin-top:\x2015%;\x0a\x20\x20}\x0a\x20\x20.aros-modal-content\x20img\x20{\x20width:\x20300px;\x20height:\x20300px;\x20}\x0a\x20\x20.aros-modal-actions\x20{\x20padding:\x2018px\x200;\x20}\x0a\x20\x20.aros-modal-button\x20{\x20width:\x20100px;\x20height:\x2040px;\x20word-break:\x20keep-all;\x20font-size:\x2012px;\x20}\x0a}\x0a', styleElement = document.createElement('style');
+styleElement.innerHTML = modalStyles;
+document.head.appendChild(styleElement);
+document.addEventListener('DOMContentLoaded', function () {
+    const _0x1e9c3a = _0x4b0117;
+    var _0x31292e = document.createElement('img');
+    _0x31292e.src = 'https://i.imgur.com/R27ROap.png';
+    _0x31292e.alt = '로고';
+    _0x31292e.className = 'logo';
+    var _0x13c5c5 = document.querySelector('.aros-modal-content');
+    _0x13c5c5 && _0x13c5c5.appendChild(_0x31292e);
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const _0x48bef5 = _0x4b0117,
+        _0x224de6 = document.getElementById('aros-modal'),
+        _0x2619ff = document.getElementById('aros-back');
+    document.addEventListener('scroll', function () {
+        const _0x492f9e = _0x48bef5,
+            _0x303318 = window.innerHeight,
+            _0x17e109 = document.documentElement.scrollHeight - window.scrollY,
+            _0x5c1b99 = _0x303318 / _0x17e109 * 0x64;
+        _0x5c1b99 >= 0x32 && (_0x224de6.style.display = 'flex');
+    });
+    _0x2619ff.addEventListener('click', function () {
+        const _0x26505f = _0x48bef5;
+        _0x224de6.style.display = 'none';
+        localStorage.setItem('modalDismissedTime', new Date().getTime());
+    });
+});
